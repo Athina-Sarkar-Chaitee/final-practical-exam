1a6d5f1ed8d9d5dcdc3847ca6b0f86fb5d02c855
Athina Sarkar <athinasarkarchaitee@Athinas-MacBook-Air.local>
2025-01-08 17:35:33 +0600

    button added

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ function find(){
 
     fetch(url)
     .then(res => res.json())
-    .then(data => find2(data));
+    .then(data => {find2(data); show(data)});
+
 
 }
 
@@ -22,17 +23,14 @@ function find2(data){
         for(let i = 1; i<= 5; i++){
 
             let newDiv = document.createElement("div");
-            newDiv.innerHTML = `Meal id: ${allMeal[i-1].idMeal} <br> Meal Name: ${allMeal[i-1].strMeal} <br> <img src="${allMeal[i-1].strMealThumb}"> <br> Cooking Instruction: <br> ${allMeal[i-1].strInstructions} ` ;
+            newDiv.innerHTML = `Meal id: ${allMeal[i-1].idMeal} <br> Meal Name: ${allMeal[i-1].strMeal} <br> <img src="${allMeal[i-1].strMealThumb}"> <br> Cooking Instruction: <br> ${allMeal[i-1].strInstructions} <br> <button onclick="show()">Show More</button> ` ;
     
     
             newDiv.classList.add("style");
             disArea.appendChild(newDiv);
         }
-        let newDiv2 = document.createElement("div");
-        newDiv2.innerHTML = `<button onclick="show()">Show More</button>` ; 
+       
 
-        newDiv2.classList.add("style2");
-        disArea.appendChild(newDiv2);
 
     }
 
@@ -57,6 +55,21 @@ function find2(data){
 }
 }
 
-function show(){
-    
-}
+// function show(data){
+
+//     let allMeal = data.meals;
+
+//     disArea = document.getElementById("dis");
+//     disArea.textContent = "" ;
+
+//     for(let i = 6; i<= allMeal.length; i++){
+
+//         let newDiv3 = document.createElement("div");
+//         newDiv3.innerHTML = `Meal id: ${allMeal[i-1].idMeal} <br> Meal Name: ${allMeal[i-1].strMeal} <br> <img src="${allMeal[i-1].strMealThumb}"> <br> Cooking Instruction: <br> ${allMeal[i-1].strInstructions}` ;
+
+
+//         newDiv3.classList.add("style");
+//         disArea.appendChild(newDiv3);
+//     }
+
+// }
