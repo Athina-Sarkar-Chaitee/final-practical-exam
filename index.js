269df7afafaf18c30b264674914631725ab9f0c3
Athina Sarkar <athinasarkarchaitee@Athinas-MacBook-Air.local>
2025-01-08 17:47:03 +0600

    button working

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function find(){
 
     fetch(url)
     .then(res => res.json())
-    .then(data => {find2(data); show(data)});
+    .then(data => find2(data));
 
 
 }
@@ -54,22 +54,34 @@ function find2(data){
 
 }
 }
+function show(){
 
-// function show(data){
+    let name = document.getElementById("name").value ;
 
-//     let allMeal = data.meals;
-
-//     disArea = document.getElementById("dis");
-//     disArea.textContent = "" ;
-
-//     for(let i = 6; i<= allMeal.length; i++){
-
-//         let newDiv3 = document.createElement("div");
-//         newDiv3.innerHTML = `Meal id: ${allMeal[i-1].idMeal} <br> Meal Name: ${allMeal[i-1].strMeal} <br> <img src="${allMeal[i-1].strMealThumb}"> <br> Cooking Instruction: <br> ${allMeal[i-1].strInstructions}` ;
+    let url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${name}` ;
 
 
-//         newDiv3.classList.add("style");
-//         disArea.appendChild(newDiv3);
-//     }
+    fetch(url)
+    .then(res => res.json())
+    .then(data =>show2(data));
 
-// }
+
+}
+function show2(data){
+
+    let allMeal2 = data.meals;
+
+    disArea = document.getElementById("dis");
+    
+
+    for(let i = 6; i<= allMeal2.length; i++){
+
+        let newDiv3 = document.createElement("div");
+        newDiv3.innerHTML = `Meal id: ${allMeal2[i-1].idMeal} <br> Meal Name: ${allMeal2[i-1].strMeal} <br> <img src="${allMeal2[i-1].strMealThumb}"> <br> Cooking Instruction: <br> ${allMeal2[i-1].strInstructions}` ;
+
+
+        newDiv3.classList.add("style");
+        disArea.appendChild(newDiv3);
+    }
+
+}
